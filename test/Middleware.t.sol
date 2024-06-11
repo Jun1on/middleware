427e27255980623ca9c622128d0779f04ef9e9a3
Junion <junionreal@gmail.com>
2024-06-11 15:42:17 -0400

    add remappings

--- test/Middleware.t.sol
+++ test/Middleware.t.sol
@@ -2,23 +2,23 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {Middleware} from "../src/Middleware.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract MiddlewareTest is Test {
+    Middleware public middleware;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        middleware = new Middleware();
+        middleware.setNumber(0);
     }
 
     function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+        middleware.increment();
+        assertEq(middleware.number(), 1);
     }
 
     function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+        middleware.setNumber(x);
+        assertEq(middleware.number(), x);
     }
 }
